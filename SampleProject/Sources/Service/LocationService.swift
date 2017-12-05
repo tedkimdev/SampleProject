768623cda7387237a964e043a2aa067850812324
aney <yenafirst91@gmail.com>
2017-12-05 14:25:28 -0500

    Add LocationService

---
+++ SampleProject/Sources/Service/LocationService.swift
@@ -0,0 +1,81 @@
+//
+//  LocationService.swift
+//  SampleProject
+//
+//  Created by aney on 2017. 12. 5..
+//  Copyright © 2017년 Ted Kim. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+protocol LocationServiceDelegate {
+  func getLocation(currentLocation: CLLocation)
+  func getLocationDidFailWithError(error: Error)
+}
+
+
+final class LocationService: NSObject {
+  
+  // MARK: Initializing
+  
+  private override init() {}
+  static let shared = LocationService()
+  
+  
+  // MARK: Properties
+  
+  let locationManager = CLLocationManager()
+  var shouldSetRegion = true
+  var lastLocation: CLLocation?
+  var delegate: LocationServiceDelegate?
+  
+  
+  // MARK: Functions
+  
+  func authorize() {
+    self.locationManager.requestWhenInUseAuthorization()
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    self.locationManager.delegate = self
+  }
+  
+  func requestLocation() {
+    self.shouldSetRegion = true
+    self.lastLocation = nil
+    
+    self.locationManager.requestLocation()
+  }
+  
+  private func updateLocation(currentLocation: CLLocation){
+    guard let delegate = self.delegate else { return }
+    delegate.getLocation(currentLocation: currentLocation)
+  }
+  
+  private func updateLocationDidFailWithError(error: Error) {
+    guard let delegate = self.delegate else { return }
+    delegate.getLocationDidFailWithError(error: error)
+  }
+  
+}
+
+
+// MARK: - CLLocationManagerDelegate
+
+extension LocationService: CLLocationManagerDelegate {
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    guard let currentLocation = locations.first,
+      self.shouldSetRegion else { return }
+  
+    self.shouldSetRegion = false
+    self.lastLocation = currentLocation
+    self.updateLocation(currentLocation: currentLocation)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    print("Failed to find location: \(error)")
+    self.updateLocationDidFailWithError(error: error)
+  }
+  
+}
