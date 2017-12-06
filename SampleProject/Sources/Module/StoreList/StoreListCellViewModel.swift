5d61ee70af8decb6d898877f890032b312bb98e4
aney <yenafirst91@gmail.com>
2017-12-05 21:18:43 -0500

    Add StoreListCellViewModel

---
+++ SampleProject/Sources/Module/StoreList/StoreListCellViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  StoreListCellViewModel.swift
+//  SampleProject
+//
+//  Created by aney on 2017. 12. 5..
+//  Copyright © 2017년 Ted Kim. All rights reserved.
+//
+
+import Foundation
+
+struct StoreListCellViewModel {
+  
+  let id: String
+  let name: String
+  let location: String
+  let displayPhone: String
+  let imageURL: String
+  
+  init(model: Business) {
+    self.id = model.id
+    self.name = model.name + "(\(model.rating))"
+    self.location = model.location
+    self.displayPhone = model.displayPhone
+    self.imageURL = model.imageURL
+  }
+  
+}
